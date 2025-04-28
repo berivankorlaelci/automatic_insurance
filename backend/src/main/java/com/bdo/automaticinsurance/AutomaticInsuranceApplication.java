f9d5864c552b6f6edb6afd05396dc9176929c087
berivankorlaelci <medusaiksiri@gmail.com>
2025-04-28 03:12:58 +0300

    initial commit

---
+++ backend/src/main/java/com/bdo/automaticinsurance/AutomaticInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.bdo.automaticinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticInsuranceApplication.class, args);
+    }
+
+}
