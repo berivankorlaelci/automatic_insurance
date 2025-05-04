c4b2a10eb3c865aaf6cb9ef8c96cf299b4751975
Umay <umayece0@gmail.com>
2025-05-05 02:37:54 +0300

    backend is configured, all bugs are fixed and updated, frontend is now more contemporary

--- backend/src/main/java/com/bdo/automaticinsurance/controller/UserInfoController.java
+++ backend/src/main/java/com/bdo/automaticinsurance/controller/UserInfoController.java
@@ -3,19 +3,26 @@ package com.bdo.automaticinsurance.controller;
 import com.bdo.automaticinsurance.model.UserInfo;
 import com.bdo.automaticinsurance.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "*") // Angular için açık bırakıyoruz
+@CrossOrigin(origins = "*") // Angular için cors ayarı
 public class UserInfoController {
 
     @Autowired
     private UserInfoService userInfoService;
 
     @PostMapping("/evaluate")
-    public UserInfo evaluateUser(@RequestBody UserInfo userInfo) {
-        return userInfoService.evaluateCredit(userInfo);
+    public ResponseEntity<UserInfo> evaluateUser(@RequestBody UserInfo userInfo) {
+        UserInfo result = userInfoService.evaluateCredit(userInfo);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<String> checkStatus() {
+        return ResponseEntity.ok("Service is running");
     }
 }
 
